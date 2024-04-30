06a91bb5e0a8ba1403382c6ea1d3f4678a0c9a1d
anarcooliks <olivierrigaut@gmail.com>
2024-04-30 20:05:56 +0200

    Control Basiques + Camera

--- Assets/Feed the Deep/Player/Src/PlayerControl.cs
+++ Assets/Feed the Deep/Player/Src/PlayerControl.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Camera cam;
+    [SerializeField] private float speed;
+    [SerializeField] Vector3 rotOffset;
+    private Rigidbody rb;
+    
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         
@@ -13,6 +21,34 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+         if (Input.GetKey(KeyCode.W)) 
+		{
+			Move();
+		}
+
+        Rotate();
+    }
+
+    private void Rotate(){
+        Vector3 playerPos = cam.WorldToScreenPoint(transform.position);
+        Vector2 mousePos = Input.mousePosition;
+        mousePos.x = mousePos.x - playerPos.x;
+        mousePos.y = mousePos.y - playerPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         
+        transform.rotation = Quaternion.Euler(0, cam.transform.rotation.z, 0) * Quaternion.Euler(new Vector3(0, -angle, 0)) * Quaternion.Euler(rotOffset);
+    }
+
+     private void Move(){
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+             float distBetweenMouseAndPlayer = Vector3.Distance(transform.position, hit.point);
+             if(distBetweenMouseAndPlayer > 1.5f){
+                    transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * speed);
+            }               
+        }
     }
 }
