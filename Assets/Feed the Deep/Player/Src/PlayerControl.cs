cb653c139e00676a82d384fde6385283e1539051
anarcooliks <olivierrigaut@gmail.com>
2024-05-06 18:26:43 +0200

    ControlV2

--- Assets/Feed the Deep/Player/Src/PlayerControl.cs
+++ Assets/Feed the Deep/Player/Src/PlayerControl.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
@@ -8,25 +10,52 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] Vector3 rotOffset;
     private Rigidbody rb;
+    private Animator animator;
+    private AimingLine aimingLine;
+
+    public float width = 0.1f;
+	public Color color = Color.red;
     
     void Awake(){
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        aimingLine = GetComponentInChildren<AimingLine>();
+        HideAimingLine();
     }
 
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Rotate();
          if (Input.GetKey(KeyCode.W)) 
 		{
 			Move();
 		}
+        else{
+            if(animator.GetBool("run") == true){
+                animator.SetBool("run", false);
+            }
+        }   
+        if (Input.GetMouseButtonDown(1)){
+            ShowAimingLine();
+        }   
+        else if (Input.GetMouseButtonUp(1)){
+            HideAimingLine();
+        }
+    }
 
-        Rotate();
+    private void ShowAimingLine(){
+        
+        aimingLine.gameObject.SetActive(true);
+    }
+
+     private void HideAimingLine(){
+        
+        aimingLine.gameObject.SetActive(false);
     }
 
     private void Rotate(){
@@ -45,9 +74,17 @@ public class PlayerControl : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
+            Vector3 target = new Vector3();
              float distBetweenMouseAndPlayer = Vector3.Distance(transform.position, hit.point);
+             target.x = hit.point.x;
+             target.y = 0f;
+             target.z = hit.point.z; 
              if(distBetweenMouseAndPlayer > 1.5f){
-                    transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * speed);
+                animator.SetBool("run", true);
+                transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+            }
+            else{
+                animator.SetBool("run", false);
             }               
         }
     }
